95cd93dd05ddbc1bad2c12dcb230d9105694dd35
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2017-07-09 02:47:09 +0900

    material uiのimport、ヘッダー設定

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import MyAwesomeReactComponent from './components/MyAwesomeReactComponent';
+import Header from './components/Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <MuiThemeProvider>
+          <Header />
+        </MuiThemeProvider>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <MuiThemeProvider>
+          <MyAwesomeReactComponent />
+        </MuiThemeProvider>
+
+        <div className="App-footer">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Written by React</p>
+        </div>
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const AppBarIcon = ()=>(
+  <AppBar
+    title="khigashi Resume"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+
+export default AppBarIcon;
